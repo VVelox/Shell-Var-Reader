5aa6e0b5fd6d9f27e34692cbcd5f9f91fe9b8c47
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-10-03 15:43:00 -0500

    mostly done... just need some docs finalized

--- lib/Shell/Var/Reader.pm
+++ lib/Shell/Var/Reader.pm
@@ -7,7 +7,7 @@ use File::Slurp qw(read_file);
 
 =head1 NAME
 
-Shell::Var::Reader - The great new Shell::Var::Reader!
+Shell::Var::Reader - Runs a sh or bash script and returns the variables that have been set as well as their values.
 
 =head1 VERSION
 
@@ -19,23 +19,29 @@ our $VERSION = '0.0.1';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Lets say '/usr/local/etc/someconfig.conf' which is basically a shell
+config and read via include in a sh or bash script, this can be used for
+getting a hash ref conttaining them.
 
-Perhaps a little code snippet.
+Similarly on systems like FreeBSD, this is also useful for reading '/etc/rc.conf'.
 
     use Shell::Var::Reader;
+    use Data::Dumper;
 
-    my $foo = Shell::Var::Reader->new();
-    ...
+    my $found_vars=Shell::Var::Reader->read_in('/usr/local/etc/someconfig.conf');
 
+    print Dumper($found_vars);
 
-=head1 SUBROUTINES/METHODS
+=head1 SUBROUTINES
 
-=head2 function1
+=head2 read_in
+
+This runs a specified file as a include and then figures out what the
+new variables are. fetching them.
 
 =cut
 
-sub read_in_shell_vars {
+sub read_in {
 	my $file = $_[1];
 
 	if ( !defined($file) ) {
@@ -49,39 +55,76 @@ sub read_in_shell_vars {
 	# figure out if we are using bash or not
 	my $raw_file  = read_file($file) or die 'Failed to read "' . $file . '"';
 	my @raw_split = split( /\n/, $raw_file );
-	my $shell      = 'sh';
+	my $shell     = 'sh';
 	if ( defined( $raw_split[0] ) && ( $raw_split[0] =~ /^\#\!.*bash/ ) ) {
-		$shell='bash';
+		$shell = 'bash';
 	}
 
 	#
 	# figure out what variables already exist...
 	#
-	my $cmd=$shell." -c 'if [ -z \"\$BASH_VERSION\" ]; then set; else set -o posix; set; fi'";
-	my $results=`$cmd`;
-	my $base_vars={};
-	my @results_split=split(/\n/, $results);
+	my $cmd           = $shell . " -c 'if [ -z \"\$BASH_VERSION\" ]; then set; else set -o posix; set; fi'";
+	my $results       = `$cmd`;
+	my $base_vars     = {'ShellVarReaderFile'=>1};
+	my @results_split = split( /\n/, $results );
 	foreach my $line (@results_split) {
-		if ($line =~ /^[\_a-zA-Z]+[\_a-zA-Z0-9]\=/) {
-			my @line_split=split(/=/,$line, 2);
-			$base_vars->{$line_split[0]}=1;
+		if ( $line =~ /^[\_a-zA-Z]+[\_a-zA-Z0-9]*\=/ ) {
+			my @line_split = split( /=/, $line, 2 );
+			$base_vars->{ $line_split[0] } = 1;
 		}
 	}
 
 	#
 	# Figure out what has been set
 	#
-	$ENV{ShellVarReaderFile}=$file;
-	$cmd=$shell." -c ' \"\$ShellVarReaderFile\" if [ -z \"\$BASH_VERSION\" ]; then set; else set -o posix; set; fi'";
-	$results=`$cmd`;
-	my $found_vars={};
-	@results_split=split(/\n/, $results);
+	$ENV{ShellVarReaderFile} = $file;
+	$cmd
+		= $shell . " -c ' . \"\$ShellVarReaderFile\" > /dev/null 2> /dev/null ; if [ -z \"\$BASH_VERSION\" ]; then set; else set -o posix; set; fi'";
+	$results = `$cmd`;
+	my $found_vars = {};
+	@results_split = split( /\n/, $results );
+	my $multiline = 0;
+	my $var_key;
+
 	foreach my $line (@results_split) {
-		if ($line =~ /^[\_a-zA-Z]+[\_a-zA-Z0-9]\=/) {
-			my @line_split=split(/=/,$line, 2);
+		if ( $line =~ /^[\_a-zA-Z]+[\_a-zA-Z0-9]*\=/ && !$multiline ) {
+			my @line_split = split( /=/, $line, 2 );
+			$var_key = $line_split[0];
+			$found_vars->{$var_key} = $line_split[1];
+
+			# if the value starts with a ' and does not end with a '
+			# it is going to be multiline
+			if (   $found_vars->{$var_key} =~ /^\'/
+				&& $found_vars->{$var_key} !~ /\'$/ )
+			{
+				$found_vars->{$var_key} =~ s/^\'//;
+				$multiline = 1;
+			}else {
+				$found_vars->{$var_key} =~ s/^\'//;
+				$found_vars->{$var_key} =~ s/\'$//;
+			}
+		}
+		else {
+			$found_vars->{$var_key} = $line;
+			# if it ends with ', then we have reached the end of the variable
+			if ( $found_vars->{$var_key} =~ /\'$/ ) {
+				$found_vars->{$var_key} =~ s/\'$//;
+				$multiline = 0;
+			}
 		}
 	}
 
+	#
+	# remove base vars
+	#
+	my @found_keys=keys(%{ $found_vars });
+	foreach my $var_key (@found_keys) {
+		if (defined($base_vars->{$var_key})) {
+			delete $found_vars->{$var_key};
+		}
+	}
+
+	return $found_vars;
 }
 
 =head1 AUTHOR
